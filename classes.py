18714824f9c368ffe4205439ba55d4eb48ff139d
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2021-01-31 18:20:22 -0500

    Add multi-part file upload

--- classes.py
+++ classes.py
@@ -1,3 +1,8 @@
 class Message:
-    def __init__(self, content=""):
-       self.content = content
+    def __init__(self, content="", file_path=None):
+        self.content = content
+        if file_path is None:
+            self.multipart = False
+        else:
+            self.multipart = True
+            self.file_path = file_path
