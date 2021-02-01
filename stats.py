e01cacd565534397bb58406d5b6a740106abcaee
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2021-02-01 13:17:12 -0500

    Improve success-histogram plotting
    Bar graph gives something closer to what I want.
    I also allow the bar graph to have negative
    score values since they were getting crushed
    to 0 despite being useful information since
    we're not taking into account bonus successes.

--- stats.py
+++ stats.py
@@ -8,21 +8,45 @@ def success_histogram(sample_size, attr, n, bins=0, path='/tmp/stats.png'):
     """
     data = np.empty(sample_size)
     for i in range(sample_size):
-        data[i] = roller.roll(attr, 0, n).total
+        data[i] = roller.roll(attr, 0, n, min_success=-attr-n).total
 
-    if bins <= 0:
-        bins = int(max(data))
-
+    # neat trick with np.unique for data for bar graph
+    labels, counts = np.unique(data, return_counts=True)
     fig, ax = plt.subplots(1,1)
 
     ax.set_title("Attribute score = {attr}, {n}d10; {samples} {sample_noun}".format(attr=attr, n=n, die_noun='die' if n==1 else 'dice', samples=sample_size, sample_noun='sample' if sample_size == 1 else 'samples'))
     ax.set_xlabel("Successes")
     ax.set_ylabel("Instances")
-    ax.hist(data, bins=bins)
+    ax.set_xticks(labels)
 
-    # neat trick with np.unique for data for bar graph
-    #labels, counts = np.unique(data, return_counts=True)
-    #ax.bar(labels, counts, align='center')
+    # https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/barchart.html
+    rects = ax.bar(labels, counts, align='center')
+    for rect in rects:
+        h = rect.get_height()
+        w = rect.get_width()
+        percentage = h / sample_size * 100
+
+        # annotate each rect with a label
+        ax.annotate('{:.1f}%'.format(percentage),
+                    xy=(rect.get_x() + w / 2, h), # xy coord tuple of annotation
+                    xytext=(0,3),                 # offset of text
+                    textcoords="offset points",   #
+                    ha='center', # center horizontal alignment
+                    va='bottom'  # bottom vertical alignment
+                    )
+        ax.annotate('{}'.format(h),
+                    xy=(rect.get_x() + w / 2, h), # xy coord tuple of annotation
+                    xytext=(0,12),                # offset of text
+                    textcoords="offset points",   #
+                    ha='center', # center horizontal alignment
+                    va='bottom'  # bottom vertical alignment
+                    )
+
+    ax.set_ymargin(0.15) # add 15% extra space at top to fit labels
+
+    # increase figure width with more data
+    # TODO figure out a more elegant way to handle this
+    fig.set_figwidth(1 * len(labels), forward=True)
 
     plt.savefig(path)
     return path
